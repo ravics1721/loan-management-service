6c86aa7d669680831515a1c26cc06d3b95a488a2
Ravi Bharti <ravics1718@gmail.com>
2021-08-11 10:41:18 +0530

    Created Routes

---
+++ routes/route.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"loan-management-service.com/lms/controllers"
+)
+
+func Router(ginMode string) *gin.Engine {
+	gin.SetMode(ginMode)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	// router.Use() //Todo: Add cors middleware
+	router.GET("/", controllers.DefaultRequest)
+	loanRoutes := router.Group("/loans")
+	{
+		loanRoutes.POST("/", controllers.AddNewLoan)           // Create new Loan
+		loanRoutes.GET("/", controllers.GetLoans)              // Get all loans or filter loans array
+		loanRoutes.GET("/:id", controllers.GetLoanById)        //Get loan by id
+		loanRoutes.PATCH("/:id", controllers.UpdateLoanStatus) //Update loan status
+		loanRoutes.DELETE("/:id", controllers.CancelLoan)      //Cancel loan
+	}
+
+	return router
+}
